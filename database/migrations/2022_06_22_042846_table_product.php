7dfdc4784f263cdd00fce594fbd83e65b1ac140a
gabfani <smgabriellafanii@gmail.com>
2022-06-23 14:13:02 +0700

    hai

---
+++ database/migrations/2022_06_22_042846_table_product.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TableProduct extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("product_name", 255)->nullable();
+            $table->string("product_code", 255)->nullable();
+            $table->string("product_photo", 255)->nullable();
+            $table->string("product_price", 255)->nullable();
+            $table->string("flag")->nullable();
+            $table->string("description")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
